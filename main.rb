b7da8430f5103e245e6f8c8c7260144c25c05528
juroga_new <juanjrgast@gmail.com>
2023-01-21 20:09:42 +0100

    Added #get_posible_coordinates, #choose_path and #play. Not the program is able to reach the objective and show the moves, but it might not be the most efficient path yet

--- main.rb
+++ main.rb
@@ -8,7 +8,7 @@ class Board
 end
 
 class Knight
-    attr_accessor :counter, :arr, :initial_position, :actual_position, :final_position
+    attr_accessor :counter, :arr, :initial_position, :actual_position, :final_position, :visited_queue, :coordinate_list
 
     MOVEMENTS = [[1,2],[2,1],[-1,2],[1,-2],[2,-1],[-2,1],[-1,-2],[-2,-1]]
 
@@ -18,23 +18,42 @@ class Knight
         @final_position = final_position
         @actual_position = initial_position
         @arr = Board.new
+        @coordinate_list = []
+        @visited_queue = []
     end
 
     def get_posible_coordinates
-        coordinate_list = MOVEMENTS.map { |a, b|  [@initial_position[0]-a, @initial_position[1]-b] }
-        coordinate_list.select! {|a,b| a > 0 && a < 9 && b > 0 && b < 9}
-        p coordinate_list
+        @coordinate_list = MOVEMENTS.map { |a, b|  [@actual_position[0]-a, @actual_position[1]-b] }
+        @coordinate_list.select! {|a,b| a > 0 && a < 9 && b > 0 && b < 9}
+        @coordinate_list - @visited_queue
+    end
+
+    def choose_path(coordinate_list)
+        if @coordinate_list.include?(@final_position)
+            @counter += 1
+            return
+        else
+            @counter += 1
+            @visited_queue.push(@actual_position)
+            @actual_position = @coordinate_list.shift
+            @coordinate_list = get_posible_coordinates
+            choose_path(@coordinate_list)
+        end
+    end
+
+    def play
+        get_posible_coordinates
+        choose_path(@coordinate_list)
     end
 end
 
 def knights_travels(initial_position, final_position)
     knight = Knight.new(initial_position, final_position)
-    knight.get_posible_coordinates
-    return puts "You can get there in #{knight.counter} moves" if initial_position == final_position
-
+    knight.play
+    return puts "You can get there in #{knight.counter} moves" if @initial_position == @final_position
 end
 
-knights_travels([1,1], [2,3])
+knights_travels([1,1], [3,8])
 
 
 
