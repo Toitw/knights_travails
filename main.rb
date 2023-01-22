4db68e3213e3ed7fe968d1344823638dc4f84f50
juroga_new <juanjrgast@gmail.com>
2023-01-22 12:51:47 +0100

    Added methods so it keeps and show correctly the counter and path, but is not working properly to finde the shortest. Branching here to try a new solution

--- main.rb
+++ main.rb
@@ -8,18 +8,21 @@ class Board
 end
 
 class Knight
-    attr_accessor :counter, :arr, :initial_position, :actual_position, :final_position, :visited_queue, :coordinate_list
+    attr_accessor :counter, :arr, :initial_position, :actual_position, :final_position, :visited_queue, :coordinate_list, :counter_list, :path, :shortest_path
 
     MOVEMENTS = [[1,2],[2,1],[-1,2],[1,-2],[2,-1],[-2,1],[-1,-2],[-2,-1]]
 
     def initialize(initial_position, final_position)
         @counter = 0
+        @counter_list = []
         @initial_position = initial_position
         @final_position = final_position
         @actual_position = initial_position
         @arr = Board.new
         @coordinate_list = []
         @visited_queue = []
+        @path = []
+        @shortest_path = []
     end
 
     def get_posible_coordinates
@@ -29,31 +32,55 @@ class Knight
     end
 
     def choose_path(coordinate_list)
-        if @coordinate_list.include?(@final_position)
-            @counter += 1
-            return
-        else
-            @counter += 1
-            @visited_queue.push(@actual_position)
-            @actual_position = @coordinate_list.shift
-            @coordinate_list = get_posible_coordinates
-            choose_path(@coordinate_list)
+        @coordinate_list.map do |coordinate|
+            if @coordinate_list.include?(@final_position)
+                @path.push(@actual_position, @final_position)
+                @counter += 1
+            else
+                @path.push(@actual_position)
+                @counter += 1
+                @visited_queue.push(@actual_position)
+                @actual_position = coordinate
+                @coordinate_list = get_posible_coordinates
+                choose_path(@coordinate_list)
+            end
+            @counter_list.push(@counter)
+            @counter = 0
+            save_shortest_path
         end
+    end
+
+    def save_shortest_path
+        if @shortest_path.length == 0
+            @shortest_path = @path
+        elsif @path.length < @shortest_path.length
+            @shortest_path = @path
+        end 
+        @path = []
+    end
+
+    def return_shortest_count
+        @shortest_count = @counter_list.min.to_i
     end
 
     def play
         get_posible_coordinates
         choose_path(@coordinate_list)
     end
+
+    def play_loop
+
+        
+    end
 end
 
 def knights_travels(initial_position, final_position)
     knight = Knight.new(initial_position, final_position)
     knight.play
-    return puts "You can get there in #{knight.counter} moves" if @initial_position == @final_position
+    return puts "You can get there in #{knight.return_shortest_count} moves, you path was #{knight.shortest_path}" if @initial_position == @final_position
 end
 
-knights_travels([1,1], [3,8])
+knights_travels([1,1], [3,5])
 
 
 
