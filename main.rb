0a329f9ccaec0cc1e1c1c90fbb5f290f56e73879
juroga_new <juanjrgast@gmail.com>
2023-01-22 19:48:38 +0100

    Can search for the final node in a breadth first search, still have to add the tracking. No using nodes yet, only variables

--- main.rb
+++ main.rb
@@ -8,54 +8,46 @@ class Board
 end
 
 class Knight
-    attr_accessor :counter, :arr, :initial_position, :actual_position, :final_position, :visited_queue, :coordinate_list, :path, :counter_list, :shortest_path
+    attr_accessor :children, :position, :visited_queue, :final_position
 
     MOVEMENTS = [[1,2],[2,1],[-1,2],[1,-2],[2,-1],[-2,1],[-1,-2],[-2,-1]]
 
-    def initialize(initial_position, final_position)
-        @counter = 0
-        @initial_position = initial_position
-        @final_position = final_position
-        @actual_position = initial_position
-        @arr = Board.new
-        @coordinate_list = @initial_position
+    def initialize(position, final_position)
+        @children = []
         @visited_queue = []
+        @position = position
+        @final_position = final_position
+        @queue = []
         @path = []
-        @counter_list = []
-        @shortest_path = []
+        @board = Board.new.arr
     end
 
     def get_posible_coordinates
-        @coordinate_list = MOVEMENTS.map { |a, b|  [@actual_position[0]-a, @actual_position[1]-b] }
-        @coordinate_list.select! {|a,b| a > 0 && a < 9 && b > 0 && b < 9}
-        @coordinate_list = @coordinate_list - @visited_queue - @actual_position
+        @children = MOVEMENTS.map { |a, b|  [@position[0]-a, @position[1]-b] }
+        @children.select! {|a,b| a > 0 && a < 9 && b > 0 && b < 9}
+        @children = @children - @visited_queue
     end
 
-    def choose_path(coordinate_list)
-        if @coordinate_list.include?(@final_position)
-            @path.push(@actual_position, @final_position)
-            @counter += 1
-            @counter_list.push(@counter)
-            @counter -= 1
-            check_shortest_path
-            return
-        else
-            @counter += 1
-            @path.push(@actual_position)
-            @visited_queue.push(@actual_position)
-            @coordinate_list.map do |coordinate|
-                @actual_position = coordinate
-                @coordinate_list = @coordinate_list - @actual_position
-                get_posible_coordinates
-                choose_path(@coordinate_list)
+    def traverse
+        @queue.push(@position)
+        @visited_queue.push(@position)
+        while @queue.empty? == false
+            @position = @queue.shift
+            if @position == @final_position
+                return "llegué"
+            end
+            @children = get_posible_coordinates
+            @children.map do |child| 
+                if !@visited_queue.include?(child)
+                    @visited_queue.push(child)
+                    @queue.push(child)
+                end
             end
         end
     end
 
     def play
-        get_posible_coordinates
-        choose_path(@coordinate_list)
-        @counter = @counter_list.min.to_i
+        traverse
     end
     def check_shortest_path
         if @shortest_path.length == 0 || @path.length < @shortest_path.length
@@ -69,10 +61,9 @@ end
 def knights_travels(initial_position, final_position)
     knight = Knight.new(initial_position, final_position)
     knight.play
-    return puts "You can get there in #{knight.counter} moves and your path is #{knight.shortest_path}" if @initial_position == @final_position
 end
 
-knights_travels([1,1], [5,6])
+knights_travels([1,1], [3,2])
 
 
 
